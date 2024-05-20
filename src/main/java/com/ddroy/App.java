ed02020a93792ebac29fc0c165e79ad59c3aca00
DDR13GIT <ddroy13@gmail.com>
2024-05-20 23:21:34 +0600

    Initial commit

---
+++ src/main/java/com/ddroy/App.java
@@ -0,0 +1,13 @@
+package com.ddroy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
